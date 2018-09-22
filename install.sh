c759165fc7a2aa18718d22633c3898ce6edf37cb
Kamiya Keisuke <luckey.star123@gmail.com>
2018-09-23 02:19:50 +0900

    Initial commit

---
+++ install.sh
@@ -0,0 +1,11 @@
+# copy .bash_profile
+cp ~/config
+cp .bash_profile ~/.bash_profile
+
+# copy dsp.service
+cp dsp.service /etc/systemd/system/.
+
+# copy Makefile-go
+cp Makefile-golang .adtech-compe-2018-e/server/Makefile
+
+
