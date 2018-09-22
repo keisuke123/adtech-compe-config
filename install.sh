ed020ee358294614c777a849c5a9b1e0bd928451
Kamiya Keisuke <luckey.star123@gmail.com>
2018-09-23 02:30:33 +0900

    copy Makefile-golang from home directory

--- install.sh
+++ install.sh
@@ -8,4 +8,4 @@ sudo cp dsp.service /etc/systemd/system/
 # copy Makefile-go
 cd ~/adtech-compe-2018-e/
 git checkout feature/server
-cp Makefile-golang ~/adtech-compe-2018-e/server/Makefile
+cp ~/config/Makefile-golang ~/adtech-compe-2018-e/server/Makefile
