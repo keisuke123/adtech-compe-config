bc6fb530b189152edde89a034ccb722985d5feb7
Kamiya Keisuke <luckey.star123@gmail.com>
2018-09-23 02:25:09 +0900

    require super user

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ cd ~/config
 cp .bash_profile ~/.bash_profile
 
 # copy dsp.service
-cp dsp.service /etc/systemd/system/
+sudo cp dsp.service /etc/systemd/system/
 
 # copy Makefile-go
 cp Makefile-golang .adtech-compe-2018-e/server/Makefile
